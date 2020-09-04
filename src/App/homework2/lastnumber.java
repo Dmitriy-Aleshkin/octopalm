7171c35e948550481dd11073d3879b6a13b098cb
Dmitriy-Aleshkin <vhfbMv9sc1>
2020-09-04 22:08:55 +0400

    home

--- src/App/homework2/lastnumber.java
+++ src/App/homework2/lastnumber.java
@@ -2,16 +2,13 @@ package App.homework2;
 
 import java.util.Scanner;
 
-public class Summa {
+public class lastnumber {
     public static void main(String[] args) {
         System.out.println("Введите число: ");
         Scanner scanner = new Scanner(System.in);
-        int num = scanner.nextInt();
-        int sum = 0;
-        while (num > 0) {
-            sum = sum + num % 10;
-            num = num / 10;
-        }
-        System.out.println(sum);
+        int value = scanner.nextInt();
+        System.out.println(value % 10);
     }
 }
+
+
---
+++ src/App/homework2/sumnumber.java
@@ -0,0 +1,17 @@
+package App.homework2;
+
+import java.util.Scanner;
+
+public class sumnumber {
+    public static void main(String[] args) {
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        int sum = 0;
+        while (num > 0) {
+            sum = sum + num % 10;
+            num = num / 10;
+        }
+        System.out.println(sum);
+    }
+}
