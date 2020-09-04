c38690f0c64dae0ba5b8a3befa1cc228772a9834
Dmitriy-Aleshkin <vhfbMv9sc1>
2020-09-04 22:01:08 +0400

    home

---
+++ src/App/Home.java
@@ -0,0 +1,14 @@
+package App.Home;
+
+import java.util.Scanner;
+
+public class Home {
+    public static void main(String[] args) {
+        System.out.println("Введите число: ");
+        Scanner scanner = new Scanner(System.in);
+        int value = scanner.nextInt();
+        System.out.println(value % 10);
+    }
+}
+
+
