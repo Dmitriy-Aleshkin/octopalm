863d03c187245b01f14040ab66f617a48b03675b
Dmitriy-Aleshkin <vhfbMv9sc1>
2020-09-09 21:02:56 +0400

    homeworks tmp

--- src/App/HomeWork3/Task2/Veterinar.java
+++ src/App/HomeWork3/Task2/Veterinar.java
@@ -1,6 +1,16 @@
 package App.HomeWork3.Task2;
 
 public class Veterinar {
+    public static void main(String[] args) {
+        Veterinar veterinarian = new Veterinar();
+        Animal[] animals = new Animal[3];
+        animals[0] = new Dog("Каша и кости", "Двор частного дома");
+        animals[1] = new Cat("Молоко и рыба", "Квартира");
+        animals[2] = new Horse("Сено", "Конюшня", 34);
+        for (Animal animal : animals) {
+            veterinarian.treatAnimal(animal);
+        }
+    }
     public void treatAnimal(Animal animal) {
         System.out.println(animal);
     }
