bb5c49ddf6c9fc599bc49e7d3136a711d5c78a25
Dmitriy-Aleshkin <vhfbMv9sc1>
2020-09-09 18:36:49 +0400

    homeworks tmp

---
+++ src/App/HomeWork3/Task3/Student.java
@@ -0,0 +1,24 @@
+package App.HomeWork3.Task3;
+
+public class Student {
+    String firstName; //Имя
+    String lastName; //Фамилия
+    String Group; //Группа
+    double averageMark; //Средняя оценка
+
+    public static void main(String[] args) {
+        String[] Student = {1, 2, 3};
+        public void getScholarship() {
+            for (int i; i < Student.length; i++) {
+                if (avarageMark = 5) {
+
+                }
+            }
+        }
+    }
+}
+//Создать метод getScholarship() для класса Student, который
+//возвращает сумму стипендии. Если средняя оценка студента равна
+//5, то сумма 100, иначе 80. Переопределить этот метод в классе
+//Aspirant. Если средняя оценка аспиранта равна 5, то сумма 200,
+//иначе 180.
